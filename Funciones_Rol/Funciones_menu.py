220f810cbb369854925f72130e4fc58a8108b8e6
MLS-2004 <marclosada2004@gmail.com>
2025-12-17 12:45:19 +0100

    Dia 1.2
    Funcion de menu + cambios para que funcione el menu

---
+++ Funciones_Rol/Funciones_menu.py
@@ -0,0 +1,8 @@
+def gen_menu(menu):
+    cabezera = menu["cabezera"].center(40,"=") + "\n"
+    datos = ""
+    resultado = ""
+    for i in range(len(menu["opciones"])):
+        datos += str(i + 1) + ") " +  menu["opciones"][i] + "\n"
+    return cabezera + datos
+
