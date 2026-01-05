2959053d6703198f61b49e4cc98a6f5e20358b08
MohamedAdghir <madghirettaibi.cf@iesesteveterradas.cat>
2026-01-05 05:04:19 +0100

    Add files via upload

--- Funciones_Rol/Funciones_menu.py
+++ Funciones_Rol/Funciones_menu.py
@@ -6,3 +6,12 @@ def gen_menu(menu):
         datos += str(i + 1) + ") " +  menu["opciones"][i] + "\n"
     return cabezera + datos
 
+
+#sirve para menus que pueden tener diferentes nombres.
+def gen_menu_2(menu, nombre):
+    cabezera = menu["cabezera"].format(nombre).center(40, "=") + "\n"
+    datos = ""
+    for i in range(len(menu["opciones"])):
+        datos += str(i + 1) + ") " + menu["opciones"][i] + "\n"
+    return cabezera + datos
+
